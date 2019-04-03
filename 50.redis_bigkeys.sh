0301a14c2c3e1a825c500adecde8abed4add4f3e
桃园令 <whymaths@gmail.com>
2019-04-03 10:53:44 +0800

    Create 50.redis_bigkeys.sh

---
+++ 50.redis_bigkeys.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dbs=$(redis-cli info keyspace | grep -E '^db' | awk -F':' '{print $1}' | tr '[a-z]' ' ' | xargs)
+bigkeys=""
+
+for db in `echo $dbs`; do
+    redis-cli --bigkeys -i 0.1 -n $db | tail -11 > /tmp/redis_bigkeys_result
+    size=$(cat /tmp/redis_bigkeys_result | grep 'Biggest string' | awk '{print $(NF-1)}')
+    if [[ x$bigkeys == 'x' ]]; then
+        bigkeys=`printf "db%2d: %d" $db $size 2>&1`
+    else
+        bigkeys=`printf "$bigkeys;db%2d: %d" $db $size 2>&1`
+    fi
+done
+
+echo $bigkeys | tr ';' '\n'
